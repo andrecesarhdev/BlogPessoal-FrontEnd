de778f0bce7f13e5185c5a92347367d004433877
André César <andrecesarhenrique@gmail.com>
2026-02-13 01:08:02 -0300

    Configuração das Rotas com o React Router, Estilização usuario e login

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -15,9 +15,15 @@ function Footer() {
           </p>
           <p className="text-lg"> Acesse nossas redes sociais</p>
           <div className="flex gap-2">
-            <LinkedinLogoIcon size={48} weight="bold" />
-            <InstagramLogoIcon size={48} weight="bold" />
-            <GithubLogoIcon size={48} weight="bold" />
+            <a href="https://www.linkedin.com/in/andrecesar-dev" target="_blank">
+            <LinkedinLogoIcon size={48} weight= 'bold'/>
+            </a>
+            <a href="https://www.instagram.com/andrecesarh" target="_blank">
+            <InstagramLogoIcon size={48} weight= 'bold'/>
+            </a>
+            <a href="https://www.github.com/andrecesarhdev" target="_blank">
+            <GithubLogoIcon size={48} weight= 'bold'/>
+            </a>
           </div>
         </div>
       </div>
