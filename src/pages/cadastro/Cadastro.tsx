4fb7dced650236df493e438e377dda8080f4fa0b
André César <andrecesarhenrique@gmail.com>
2026-02-19 00:26:09 -0300

    Finaliza Axios / Cadastro / Login / Context

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,44 +1,77 @@
-import { useState, type ChangeEvent } from "react";
-import type  Usuario  from "../../models/Usuario";
-import { cadastrarUsuario } from "../../services/Service";
+import { useEffect, useState, type ChangeEvent } from "react"
+import { useNavigate } from "react-router-dom"
+import { ClipLoader } from "react-spinners"
+import type { Usuario } from "../../models/Usuario"
+import { cadastrarUsuario } from "../../services/Service"
 
 function Cadastro() {
-  //esse cara vai receber o json do formulario de cadastro
+
+  const navigate = useNavigate()
+
+  const[isLoading, setIsLoading] = useState<boolean>(false)
+
+  const[confirmarSenha, setConfirmarSenha] = useState<string>("")
+
   const [usuario, setUsuario] = useState<Usuario>({
     id: 0,
-    nome: "",
-    usuario: "",
-    senha: "",
-    foto: "",
-  });
+    nome: '',
+    usuario: '',
+    senha: '',
+    foto: ''
+  })
+
+  function retornar(){
+    navigate('/')
+  }
+
+  useEffect(() => {
+    if (usuario.id !== 0){
+       
+      retornar()
+    }
+  }, [usuario])
 
   function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
     setUsuario({
       ...usuario,
-      [e.target.name]: e.target.value,
-    });
+      [e.target.name]: e.target.value
+    })
   }
-  // enviar os dados pegos do formularoop e mandar para o backend
+
+  function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
+    setConfirmarSenha(e.target.value)
+  }
+
   async function cadastrarNovoUsuario(e: ChangeEvent<HTMLFormElement>) {
-    e.preventDefault();
-    try {
-      await cadastrarUsuario("/usuarios/cadastrar", usuario, setUsuario);
-      alert("Foi, deu bom");
-    } catch (error) {
-      alert("Deu ruim");
-      console.log(error);
+    e.preventDefault()
+
+    if(confirmarSenha === usuario.senha && usuario.senha.length >= 8) {
+      
+      setIsLoading(true)
+      
+      try {
+        await cadastrarUsuario('/usuarios/cadastrar', usuario, setUsuario)
+        alert('Usuário cadastrado com sucesso!')
+      } catch (error) {
+        alert('Erro ao cadastrar o usuário!')
+      }
+    } else {
+      alert('Dados do usuário inconsistentes! Verifique as informações de cadastro.')
+      setUsuario({...usuario, senha: ''})
+      setConfirmarSenha('')
     }
+
+    setIsLoading(false)
   }
 
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
-        <div className="bg-[url('https://i.imgur.com/ZZFAmzo.jpg')] lg:block hidden bg-no-repeat w-full min-h-screen bg-cover bg-center"></div>
+        <div
+          className="bg-[url('https://i.imgur.com/ZZFAmzo.jpg')] lg:block hidden bg-no-repeat w-full min-h-screen bg-cover bg-center"
+        ></div>
 
-        <form
-          onSubmit={cadastrarNovoUsuario}
-          className="flex justify-center items-center flex-col w-2/3 gap-3"
-        >
+        <form onSubmit={cadastrarNovoUsuario} className="flex justify-center items-center flex-col w-2/3 gap-3">
           <h2 className="text-slate-900 text-5xl">Cadastrar</h2>
 
           <div className="flex flex-col w-full">
@@ -50,9 +83,7 @@ function Cadastro() {
               placeholder="Nome"
               className="border-2 border-slate-700 rounded p-2"
               value={usuario.nome}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                atualizarEstado(e)
-              }
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
 
@@ -65,9 +96,7 @@ function Cadastro() {
               placeholder="Usuario"
               className="border-2 border-slate-700 rounded p-2"
               value={usuario.usuario}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                atualizarEstado(e)
-              }
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
 
@@ -80,9 +109,7 @@ function Cadastro() {
               placeholder="Foto"
               className="border-2 border-slate-700 rounded p-2"
               value={usuario.foto}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                atualizarEstado(e)
-              }
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
 
@@ -95,9 +122,7 @@ function Cadastro() {
               placeholder="Senha"
               className="border-2 border-slate-700 rounded p-2"
               value={usuario.senha}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                atualizarEstado(e)
-              }
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
 
@@ -109,10 +134,8 @@ function Cadastro() {
               name="confirmarSenha"
               placeholder="Confirmar Senha"
               className="border-2 border-slate-700 rounded p-2"
-              // value={usuario.}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                atualizarEstado(e)
-              }
+              value={confirmarSenha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
             />
           </div>
 
@@ -120,6 +143,7 @@ function Cadastro() {
             <button
               type="reset"
               className="rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2"
+              onClick={retornar}
             >
               Cancelar
             </button>
@@ -128,13 +152,19 @@ function Cadastro() {
               type="submit"
               className="rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2 flex justify-center"
             >
-              Cadastrar
+            { isLoading ?
+              <ClipLoader
+                color="#ffffff"
+                size={24}
+              /> :
+              <span>Cadastrar</span>
+            }
             </button>
           </div>
         </form>
       </div>
     </>
-  );
+  )
 }
 
-export default Cadastro;
+export default Cadastro
