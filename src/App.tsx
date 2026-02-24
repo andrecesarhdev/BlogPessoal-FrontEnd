5675309e0003efce2e565ab5bd436da607c39191
André César <andrecesarhenrique@gmail.com>
2026-02-23 23:59:52 -0300

    Implementa Crud de Postagem / Estililo e logica perfil

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
+import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem";
+import FormPostagem from "./components/postagem/formpostagem/FormPostagem";
+import ListaPostagens from "./components/postagem/listapostagens/ListaPostagens";
+import DeletarTema from "./components/temas/deletartema/DeletarTema";
 import FormTema from "./components/temas/formtema/FormTema";
 import ListaTemas from "./components/temas/listaTemas/ListaTemas";
 import { AuthProvider } from "./contexts/AuthContext";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import DeletarTema from "./components/temas/deletartema/DeletarTema";
+import Perfil from "./pages/perfil/Perfil";
 
 function App() {
   return (
@@ -25,6 +29,11 @@ function App() {
               <Route path="/cadastrartema" element={<FormTema />} />
               <Route path="/editartema/:id" element={<FormTema />} />
               <Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,6 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
+
 // import React from "react";
 function Home() {
   return (
@@ -8,12 +11,7 @@ function Home() {
             <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
             <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
             <div className="flex justify-around gap-4">
-              <div
-                className="rounded text-whithe
-              border-white border-solid border-2 py-2 px-4"
-              >
-                Nova Postagem
-              </div>
+              <ModalPostagem />
             </div>
           </div>
           <div className="flex justify-center">
@@ -25,6 +23,7 @@ function Home() {
           </div>
         </div>
       </div>
+      <ListaPostagens />
     </>
   );
 }
